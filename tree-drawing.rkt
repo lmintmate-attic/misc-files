0dde8cbf58716dc352e6e8c0cd2651b229227a30
lmintmate <lmintmate@users.noreply.github.com>
2017-06-05 16:57:05 +0300

    added variables

--- tree-drawing.rkt
+++ tree-drawing.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (require 2htdp/image)
 (define unit 10)
-(underlay/xy (define trunk (rectangle (* unit 2) (* unit 6) "solid" "brown"))
+(define trunk (rectangle (* unit 2) (* unit 6) "solid" "brown"))
 (define leaves (ellipse (* unit 3) (* unit 6) "solid" "dark green"))
 (underlay/xy trunk
 (/ unit -2) (* unit -5)
