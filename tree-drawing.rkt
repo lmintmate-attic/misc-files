451429d0c43f6e3e4b8cd666a13de9d1e471f990
lmintmate <lmintmate@users.noreply.github.com>
2017-06-05 16:52:39 +0300

    changed color of leaves in tree

--- tree-drawing.rkt
+++ tree-drawing.rkt
@@ -4,4 +4,4 @@
 (underlay/xy
 (rectangle (* unit 2) (* unit 6) "solid" "brown")
 (/ unit -2) (* unit -5)
-(ellipse (* unit 3) (* unit 6) "solid" "green"))
+(ellipse (* unit 3) (* unit 6) "solid" "dark green"))
