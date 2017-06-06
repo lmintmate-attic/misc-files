43d1084cb69611c631f8395f0c993ab23443c44d
lmintmate <lmintmate@users.noreply.github.com>
2017-06-06 18:48:28 +0300

    new file

---
+++ shapes.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(require 2htdp/image)
+; tall shape
+;(define shape (rectangle 10 50 "outline" "black"))
+;square shape
+;(define shape (square 50 "outline" "black"))
+; wide shape
+(define shape (rectangle 50 10 "outline" "black"))
+;dimensions variable: square, tall or wide
+(define dimensions
+(if (= (image-height shape) (image-width shape)) "square"
+(if (>= (image-height shape) (image-width shape)) "tall" "wide")))
