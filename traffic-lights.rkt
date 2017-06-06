3fcf4b2ee1bc06f34b1b96095f78c02e931282de
lmintmate <lmintmate@users.noreply.github.com>
2017-06-06 17:08:12 +0300

    added traffic-lights.rkt

---
+++ traffic-lights.rkt
@@ -0,0 +1,14 @@
+#lang racket
+;Traffic lights: current color
+(define current-color "green")
+; next-color variable - prints the color that will appear next on the lights
+(define next-color
+  ;if the color is green
+  (if (string=? "green" current-color) "red" 
+      ; if the color isn't green, but yellow
+      (if (string=? "yellow" current-color) "green" 
+          ; if the color isn't yellow, but red
+          (if (string=? "red" current-color) "yellow"
+;this following last parameter is just filled in so that Racket won't throw an error in case the current-color variable is filled with anything other than "red", "yellow" or "green".
+              "Please write one of red, yellow or green in the current-color variable definition."))))
+;execute by writing next-color in the interactions panel(the REPL).
