6b781a014a5d6c2d8e30654a3d8d85c1ca2a106e
lmintmate <lmintmate@users.noreply.github.com>
2017-06-08 18:42:43 +0300

    new file mall.scm

---
+++ mall.scm
@@ -0,0 +1,7 @@
+; Introducing the problem: Suppose you want to decide whether today is an appropriate day to go to the mall. You go to the mall if it is either not sunny or if today is Friday (because that is when stores post new sales items).(from Htdp, part I, exercise 7).
+; defining the parameters: whether it is sunny or not, and whether it is Friday or not. These can be modified by hand.
+(define sunny #f)
+(define friday #t)
+; introducing the decision variable. It examines whether it isn't sunny or whether it is Friday, and decides accordingly whether to go to the mall or not.
+(define decision
+(if (or (not sunny) friday) "It's a good day to go to the mall." "It's not a good day to go to the mall."))
