116a2435ebcc3f62d201eff8870818a14b9b7849
lmintmate <lmintmate@users.noreply.github.com>
2017-06-08 09:58:46 +0300

    added printf to phrases

--- traffic-lights.rkt
+++ traffic-lights.rkt
@@ -4,11 +4,11 @@
 ; next-color variable - prints the color that will appear next on the lights
 (define next-color
   ;if the current color is green
-  (if (string=? "green" current-color) "red" 
+  (if (string=? "green" current-color) (printf "red") 
       ; if the current color isn't green, but yellow
-      (if (string=? "yellow" current-color) "green" 
+      (if (string=? "yellow" current-color) (printf "green") 
           ; if the current color isn't yellow, but red
-          (if (string=? "red" current-color) "yellow"
+          (if (string=? "red" current-color) (printf "yellow")
 ;this following last parameter is just filled in so that Racket won't throw an error in case the current-color variable is filled with anything other than "red", "yellow" or "green".
-              "Please write one of red, yellow or green in the current-color variable definition."))))
+              (printf "Please write one of red, yellow or green in the current-color variable definition.")))))
 ;execute by writing next-color in the interactions panel(the REPL).
