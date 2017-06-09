90d3ad5111b3a7973a9d5c3d3f77f57c99adffc3
lmintmate <lmintmate@users.noreply.github.com>
2017-06-09 09:47:49 +0300

    added print command

--- mall.scm
+++ mall.scm
@@ -4,4 +4,4 @@
 (define friday #t)
 ; introducing the decision variable. It examines whether it isn't sunny or whether it is Friday, and decides accordingly whether to go to the mall or not.
 (define decision
-(if (or (not sunny) friday) "It's a good day to go to the mall." "It's not a good day to go to the mall."))
+(if (or (not sunny) friday) (print "It's a good day to go to the mall.") (print "It's not a good day to go to the mall.")))
