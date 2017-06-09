b3a355008ca26e5e59b0588cf7d44be0c20d0c95
lmintmate <lmintmate@users.noreply.github.com>
2017-06-09 20:52:23 +0300

    new file

---
+++ hours-to-wages-mark2.rkt
@@ -0,0 +1,15 @@
+#lang racket
+; this is an extensive rewrite of the previous one, consolidating the if statements that verified whether the numbers were above zero, and adding a way to flag any non-numeric input
+(define rate 10)
+(define hours 45)
+(define overtime-threshold 40)
+(define compute
+; a way to flag any non-numeric input in any of the variables
+  (if (and (number? rate) (number? hours) (number? overtime-threshold))
+;the if statement that rules off 0 and negative numbers
+ (if (or (<= rate 0)(<= hours 0) (<= overtime-threshold 0)) (printf "Please write a number of hours, rate and/or overtime threshold above zero.")
+; the calculation of the wages
+  (if (<= hours overtime-threshold) (* hours rate)
+       (+ (* overtime-threshold rate) (* (- hours overtime-threshold) (* rate 1.5))))) 
+; the message that will be printed in case anything non-numeric has been inputted in any of the variables
+(printf "Please use a number for the hours, rate and/or overtime threshold.")))
