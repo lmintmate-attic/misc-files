d6bfccf5ad6badf9be81b8d9f25510bff43a7656
lmintmate <lmintmate@users.noreply.github.com>
2017-06-09 09:50:00 +0300

    added print command

--- hours-to-wages.scm
+++ hours-to-wages.scm
@@ -2,8 +2,8 @@
 (define hours 45)
 (define overtime-threshold 40)
 (define compute
- (if (<= hours 0) "Please write a number of hours, rate and/or overtime threshold above zero."
-      (if (<= rate 0) "Please write a number of hours, rate and/or overtime threshold above zero."
-          (if (<= overtime-threshold 0) "Please write a number of hours, rate and/or overtime threshold above zero."
+ (if (<= hours 0) (print "Please write a number of hours, rate and/or overtime threshold above zero.")
+      (if (<= rate 0) (print "Please write a number of hours, rate and/or overtime threshold above zero.")
+          (if (<= overtime-threshold 0) (print "Please write a number of hours, rate and/or overtime threshold above zero.")
   (if (<= hours overtime-threshold) (* hours rate)
        (+ (* overtime-threshold rate) (* (- hours overtime-threshold) (* rate 1.5))))))))
