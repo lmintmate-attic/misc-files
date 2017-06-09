db33e4f5883f547aca277bb965453b7055c598eb
lmintmate <lmintmate@users.noreply.github.com>
2017-06-09 09:46:15 +0300

    added print command

--- switching-computer.scm
+++ switching-computer.scm
@@ -1,3 +1,3 @@
 (define new-messages #t)
 (define switching-computer
-  (if new-messages "Must turn the computer on." "No need to turn the computer on."))
+  (if new-messages (print "Must turn the computer on.") (print "No need to turn the computer on.")))
