959fe1712a13caafe9c1d13d18e3362f8802c29a
lmintmate <lmintmate@users.noreply.github.com>
2017-06-09 16:44:37 +0300

    reaction in case new-messages is not a boolean
    which sets apart atm the racket version from the other 2 versions

--- switching-computer.rkt
+++ switching-computer.rkt
@@ -1,4 +1,6 @@
 #lang racket
 (define new-messages #true)
 (define switching-computer
-  (if new-messages (printf "Must turn the computer on.") (printf "No need to turn the computer on.")))
+  (if (boolean? new-messages) 
+   (if (false? new-messages) (printf "No need to turn the computer on.") (printf "Must turn the computer on."))
+   (printf "This isn't a boolean.")))
