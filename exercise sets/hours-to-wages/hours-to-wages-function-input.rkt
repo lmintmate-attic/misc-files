217731c7059024f16f3c775598e39efdb8c0ba06
lmintmate <marynik37@hotmail.gr>
2017-06-12 15:04:04 +0300

    new files

---
+++ exercise sets/hours-to-wages/hours-to-wages-function-input.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(define (compute) ;rate hours overtime-threshold)
+
+
+(printf "Input a number for the rate: ")
+(define rate (read))
+(unless (and (number? rate) (>= rate 0)) (error "Please use a number above zero for the rate."))
+
+(printf "Input a number for the hours: ")
+(define hours (read))
+(unless (and (number? hours) (>= hours 0)) (error "Please use a number above zero for the hours."))
+
+(printf "Input a number for the overtime threshold: ")
+(define overtime-threshold (read))
+(unless (and (number? overtime-threshold) (>= overtime-threshold 0)) (error "Please use a number above zero for the overtime threshold."))
+
+; the calculation of the wages
+  (if (<= hours overtime-threshold) (* hours rate)
+       (+ (* overtime-threshold rate) (* (- hours overtime-threshold) (* rate 1.5)))))
